8673c3ccd3def3584bdda7e4f12e9168c64f4b1d
dadaruto <cheladorcas.ruto@gmail.com>
2024-07-01 17:05:09 +0300

    Files Manager

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
+import express from 'express';
+import routes from './routes/index';
 import redisClient from './utils/redis';
+import dbClient from './utils/db';
 
-(async () => {
-    console.log(redisClient.isAlive());
-    console.log(await redisClient.get('myKey'));
-    await redisClient.set('myKey', 12, 5);
-    console.log(await redisClient.get('myKey'));
+const app = express();
+const port = process.env.PORT || 5000;
 
-    setTimeout(async () => {
-        console.log(await redisClient.get('myKey'));
-    }, 1000*10)
-})();
+app.use(express.json());
+app.use('/', routes);
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+  console.log(redisClient.isAlive()); // Should log true if connected
+  console.log(redisClient.get('some_key')); // Example key, should log the value or null if key doesn't exist
+  console.log(await dbClient.nbUsers()); // Should log the number of users
+  console.log(await dbClient.nbFiles()); // Should log the number of files
+});
