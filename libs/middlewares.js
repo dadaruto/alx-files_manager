a429db61cc5dc5cf06870192ff4c942fe7cb1313
dadaruto <cheladorcas.ruto@gmail.com>
2024-07-01 15:58:52 +0300

    Remove all files

--- libs/middlewares.js
+++
@@ -1,8 +0,0 @@
-import bodyParser from 'body-parser';
-
-const injectMiddlewares = (server) => {
-  server.use(bodyParser.json());
-  // Add other middlewares here
-};
-
-export default injectMiddlewares;
