4965a05c9e3613641e39d0a615836ebdbac11ed6
dadaruto <cheladorcas.ruto@gmail.com>
2024-07-01 15:52:45 +0300

    Files Manager

--- main.js
+++ main.js
@@ -1,30 +1,29 @@
+// main.js
 import dbClient from './utils/db';
 
 const waitConnection = () => {
-    return new Promise((resolve, reject) => {
-        let i = 0;
-        const repeatFct = async () => {
-            await setTimeout(() => {
-                i += 1;
-                if (i >= 10) {
-                    reject()
-                }
-                else if(!dbClient.isAlive()) {
-                    repeatFct()
-                }
-                else {
-                    resolve()
-                }
-            }, 1000);
-        };
-        repeatFct();
-    })
+  return new Promise((resolve, reject) => {
+    let i = 0;
+    const repeatFct = async () => {
+      await setTimeout(() => {
+        i += 1;
+        if (i >= 10) {
+          reject();
+        } else if (!dbClient.isAlive()) {
+          repeatFct();
+        } else {
+          resolve();
+        }
+      }, 1000);
+    };
+    repeatFct();
+  });
 };
 
 (async () => {
-    console.log(dbClient.isAlive());
-    await waitConnection();
-    console.log(dbClient.isAlive());
-    console.log(await dbClient.nbUsers());
-    console.log(await dbClient.nbFiles());
+  console.log(dbClient.isAlive()); // false initially
+  await waitConnection(); // Wait until connection is established
+  console.log(dbClient.isAlive()); // true once connected
+  console.log(await dbClient.nbUsers()); // Number of users
+  console.log(await dbClient.nbFiles()); // Number of files
 })();
