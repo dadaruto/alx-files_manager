480a40c7d4ee46d80764bc0b1d841f78cfd2f999
dadaruto <cheladorcas.ruto@gmail.com>
2024-07-03 12:43:21 +0300

    Files Manager

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
 import redisClient from './utils/redis';
 
+const checkRedisConnection = async () => {
+  return new Promise((resolve) => {
+    const checkConnection = () => {
+      if (redisClient.isAlive()) {
+        resolve(true);
+      } else {
+        setTimeout(checkConnection, 100);
+      }
+    };
+    checkConnection();
+  });
+};
+
 (async () => {
+  await checkRedisConnection();
   console.log(redisClient.isAlive());
   console.log(await redisClient.get('myKey'));
   await redisClient.set('myKey', 12, 5);
